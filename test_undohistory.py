0337a66f66297527a03fa8879337c29b5fb5c516
David Straub <straub@protonmail.com>
2024-03-18 20:36:42 +0100

    Add unit tests

--- test_undohistory.py
+++ test_undohistory.py
@@ -23,6 +23,7 @@
 import os
 import shutil
 import tempfile
+import time
 import unittest
 
 from gramps.gen.db import DbTxn, DbWriteBase
@@ -42,6 +43,13 @@ from gramps.gen.lib import (
 )
 
 DBID = "sqlite+history"
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 
 class TestUndoHistory(unittest.TestCase):
@@ -84,5 +92,61 @@ class TestUndoHistory(unittest.TestCase):
         obj = obj_class()
         add_func(obj, trans)
 
-    def test_nothing(self):
+    def _get_history_table(self, table_name):
+        """Get a table from the history database."""
+        dbundo = self.db.get_undodb()
+        with dbundo._connect() as connection:
+            connection.row_factory = dict_factory
+            cursor = connection.cursor()
+            cursor.execute(f"SELECT * FROM {table_name}")
+            return cursor.fetchall()
+
+    def test_initial_sate(self):
+        assert self.db.get_number_of_people() == 10
+        sessions = self._get_history_table("sessions")
+        assert len(sessions) == 1
+        assert sessions[0]["id"] == 1
+        assert time.time() - sessions[0]["timestamp"] / 1e9 < 10
+        transactions = self._get_history_table("transactions")
+        assert len(transactions) == 1
+        assert transactions[0]["session"] == 1
+        assert transactions[0]["id"] == 1
+        assert transactions[0]["description"] == "Add test objects"
+        assert transactions[0]["timestamp"] - sessions[0]["timestamp"] < 10e9
+        assert transactions[0]["undo"] == 0
+        commits = self._get_history_table("commits")
+        assert len(commits) == 100
+        for commit in commits:
+            assert commit["session"] == 1
+            assert commit["trans_type"] == 0  # add
+            assert commit["timestamp"] < transactions[0]["timestamp"]
+        assert len([com for com in commits if com["obj_class"] == "Person"]) == 10
+        assert len([com for com in commits if com["obj_class"] == "Family"]) == 10
+        assert len([com for com in commits if com["obj_class"] == "Event"]) == 10
+        assert len([com for com in commits if com["obj_class"] == "Place"]) == 10
+        assert len([com for com in commits if com["obj_class"] == "Repository"]) == 10
+        assert len([com for com in commits if com["obj_class"] == "Source"]) == 10
+        assert len([com for com in commits if com["obj_class"] == "Citation"]) == 10
+        assert len([com for com in commits if com["obj_class"] == "Media"]) == 10
+        assert len([com for com in commits if com["obj_class"] == "Note"]) == 10
+        assert len([com for com in commits if com["obj_class"] == "Tag"]) == 10
+
+    def test_undo_redo_initial_state(self):
+        assert self.db.get_number_of_people() == 10
+        self.db.undo()
+        sessions = self._get_history_table("sessions")
+        assert len(sessions) == 1
+        transactions = self._get_history_table("transactions")
+        assert len(transactions) == 2
+        assert transactions[1]["description"] == "_Undo Add test objects"
+        commits = self._get_history_table("commits")
+        assert len(commits) == 100
+        assert self.db.get_number_of_people() == 0
+        self.db.redo()
+        transactions = self._get_history_table("transactions")
+        assert len(transactions) == 3
+        assert transactions[1]["description"] == "_Undo Add test objects"
+        assert transactions[2]["description"] == "_Redo Add test objects"
+        commits = self._get_history_table("commits")
+        assert len(commits) == 100
         assert self.db.get_number_of_people() == 10
